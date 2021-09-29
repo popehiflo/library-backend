014c5525bb06d6304ccb03ac0bc7e3b57cee98ee
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-29 17:53:08 -0500

    Updated clases/entidades con anotaciones y relaciones

--- src/main/java/pe/popehiflo/librarybackend/model/Categoria.java
+++ src/main/java/pe/popehiflo/librarybackend/model/Categoria.java
@@ -1,14 +1,29 @@
 package pe.popehiflo.librarybackend.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
-	
+	@OneToMany(mappedBy = "categoria", fetch = FetchType.LAZY)
 	private List<Libro> libros = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/pe/popehiflo/librarybackend/model/Libro.java
+++ src/main/java/pe/popehiflo/librarybackend/model/Libro.java
@@ -1,12 +1,29 @@
 package pe.popehiflo.librarybackend.model;
 
-public class Libro {
-	
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "libros")
+public class Libro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nombreAutor;
 	private String texto;
-	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Libro() {
@@ -86,7 +103,5 @@ public class Libro {
 			return false;
 		return true;
 	}
-	
-	
 
 }
