3854ed2c194f38637611936f7afb080eaafd9c0d
popehiflo <poolpetterhijuel@hotmail.com>
2021-09-29 15:42:24 -0500

    created proyecto en spring boot - backend

---
+++ src/main/java/pe/popehiflo/librarybackend/LibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.popehiflo.librarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/popehiflo/librarybackend/LibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.popehiflo.librarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
