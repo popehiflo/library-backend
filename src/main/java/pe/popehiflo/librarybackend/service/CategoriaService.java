f1e6f4067161c217880e9e769529c05cd3656b45
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-09 22:36:46 -0500

    uso del manejo de exception personalizada de obj no encontrado

--- src/main/java/pe/popehiflo/librarybackend/service/CategoriaService.java
+++ src/main/java/pe/popehiflo/librarybackend/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import pe.popehiflo.librarybackend.model.Categoria;
 import pe.popehiflo.librarybackend.repository.CategoriaRepository;
+import pe.popehiflo.librarybackend.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,9 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> resp = repository.findById(id);
-		return resp.orElse(null);
+		return resp.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto no fue encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()
+				)
+		);
 	}
 }
