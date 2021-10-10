faf49e9dcdb1a0bcde26be1084a1bc59030f25c3
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-09 22:11:02 -0500

    feat: ObjectNotFoundExceptio, hereda de RuntimeException

---
+++ src/main/java/pe/popehiflo/librarybackend/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package pe.popehiflo.librarybackend.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
