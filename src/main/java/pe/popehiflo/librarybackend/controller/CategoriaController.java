44fd164eb6085542af552a1122d779a7d09d93e2
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-09 23:29:57 -0500

    feat: findAll para Categoria con patron DTO

--- src/main/java/pe/popehiflo/librarybackend/controller/CategoriaController.java
+++ src/main/java/pe/popehiflo/librarybackend/controller/CategoriaController.java
@@ -1,5 +1,8 @@
 package pe.popehiflo.librarybackend.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pe.popehiflo.librarybackend.dto.CategoriaDTO;
 import pe.popehiflo.librarybackend.model.Categoria;
 import pe.popehiflo.librarybackend.service.CategoriaService;
 
@@ -17,9 +21,28 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaService service;
 	
+	/**
+	 * GET Categoria por id
+	 * @param id
+	 * @return objeto Categoria con su lista de Libros
+	 */
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable("id") Integer id) {
 		Categoria categoria = service.findById(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	// DTO que listara las CATEGORIAS omitiendo los LIBROS que hay en ellas
+	/**
+	 * GET Lista de Categorias
+	 * @return lista de CATEGORIAS sin sus LIBROS
+	 */
+	@GetMapping(value = "")
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(
+				cat -> new CategoriaDTO(cat)).collect(Collectors.toList()
+		);
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/pe/popehiflo/librarybackend/service/CategoriaService.java
+++ src/main/java/pe/popehiflo/librarybackend/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package pe.popehiflo.librarybackend.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,9 @@ public class CategoriaService {
 				)
 		);
 	}
+	
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
+	
 }
