728b99a8a99176554479f132c2ac3bb4dcccca14
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-15 10:26:56 -0500

    feat: devuelve el error como una lista con campo y mensaje

--- src/main/java/pe/popehiflo/librarybackend/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/pe/popehiflo/librarybackend/controller/exceptions/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -33,5 +35,19 @@ public class ControllerExceptionHandler {
 		);
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
-
+	/**
+	 * Cuando no pasa la Validación de Entidad (ej. @NotBlank, @Length)
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+		ValidationError error = new ValidationError(
+				System.currentTimeMillis(),
+				HttpStatus.BAD_REQUEST.value(),
+				"Error en validación de campos"
+		);
+		for (FieldError field: e.getBindingResult().getFieldErrors()) {
+			error.addErrors(field.getField(), field.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 }
